627cdad9454558e8ff103e1fd0f936ab34d5e74b
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-11-26 17:44:46 +0100

    Refactor productRepository.dart

--- repository/productRepository.dart
+++ repository/productRepository.dart
@@ -1,38 +1,4 @@
 import '../models/product.dart';
-
-List<Product> fakeProducts = [
-  Product(
-    id: 1,
-    name: 'Product 1',
-    description: 'This is product 1',
-    price: 100,
-  ),
-  Product(
-    id: 2,
-    name: 'Product 2',
-    description: 'This is product 2',
-    price: 200,
-  ),
-  Product(
-    id: 3,
-    name: 'Product 3',
-    description: 'This is product 3',
-    price: 300,
-  ),
-  Product(
-    id: 4,
-    name: 'Product 4',
-    description: 'This is product 4',
-    price: 400,
-  ),
-  Product(
-    id: 5,
-    name: 'Product 5',
-    description: 'This is product 5',
-    price: 500,
-  ),
-];
-
 class ProductRepository {
   factory ProductRepository() {
     return _instance;
